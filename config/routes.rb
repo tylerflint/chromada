1f431bd45e1f8745438727147f03c08021777e14
Tyler Flint <tylerflint@Delorum10.local>
2010-04-24 11:48:25 -0600

    adding rails_upgrade plugin; created skeleton for admin helpers

--- config/routes.rb
+++ config/routes.rb
@@ -16,8 +16,8 @@ ActionController::Routing::Routes.draw do |map|
   map.namespace :admin do |admin|             
     admin.root                                    :controller => 'employees',     :action => 'dashboard'
     admin.dashboard     '/dashboard',             :controller => 'employees',     :action => 'dashboard'
-    admin.logout        '/logout',                :controller => 'sessions',      :action => 'destroy'
-    admin.login         '/login',                 :controller => 'sessions',      :action => 'new'
+    admin.logout        '/logout',                :controller => 'employees',      :action => 'logout'
+    admin.login         '/login',                 :controller => 'employees',      :action => 'login'
     admin.resources                               :employees
     admin.resource                                :session
   end
