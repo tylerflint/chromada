8208c0341f89d04bdb194d625bd18bee11ac85fb
Tyler Flint <tylerflint@Delorum10.local>
2010-05-30 20:51:00 -0600

    fixing deprecation warnings

--- app/helpers/admin/admin_helper.rb
+++ app/helpers/admin/admin_helper.rb
@@ -3,7 +3,7 @@ module Admin::AdminHelper
   def admin_grid(object, options={})
     grid = AdminGrid.new(self, object, session, params, options)
     yield grid
-    concat grid.to_html
+    raw grid.to_html
   end
   
 end
