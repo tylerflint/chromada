1c947af5b3bf5f69a2f2e944917102c147c61e25
Tyler Flint <tylerflint@Delorum10.local>
2010-06-17 20:12:51 -0600

    creating a test fixture; creating a login feature spec

---
+++ features/step_definitions/admin_login_steps.rb
@@ -0,0 +1,43 @@
+Given /^I am currently not logged in$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I click the login link$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^Enter my credentials admin@chromada\.com, password$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I am redirected to the admin dashboard$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^Enter the credentials admin@chromada\.com, wrong$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I am not redirected to the admin dashboard$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I am shown an error message$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^I am currently logged in$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I click logout$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I am redirected to the homepage$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^My admin session is cleared$/ do
+  pending # express the regexp above with the code you wish you had
+end
