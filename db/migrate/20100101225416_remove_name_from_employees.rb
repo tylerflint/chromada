3e851a3b268d6c8422b7e44f5726840ac425a185
Tyler Flint <tylerflint@Delorum10.local>
2010-07-13 21:10:58 -0600

    deleting all references to authenticated_employee

--- db/migrate/20100101225416_remove_name_from_employees.rb
+++
@@ -1,9 +0,0 @@
-class RemoveNameFromEmployees < ActiveRecord::Migration
-  def self.up
-    remove_column :employees, :name
-  end
-
-  def self.down
-    add_column :employees, :name, :string, :limit => 100, :default => '', :null => true, :after => "login"
-  end
-end
--- db/migrate/20100515210940_remove_login_from_employees.rb
+++
@@ -1,9 +0,0 @@
-class RemoveLoginFromEmployees < ActiveRecord::Migration
-  def self.up
-    remove_column :employees, :login
-  end
-
-  def self.down
-    add_column :employees, :login, :string, :limit => 40
-  end
-end
