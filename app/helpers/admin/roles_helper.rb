80eaeeb2341fefde702b91d4c4874c257286a88d
Tyler Flint <tylerflint@Delorum10.local>
2010-08-17 20:46:28 -0600

    updating role action javascript

--- app/helpers/admin/roles_helper.rb
+++ app/helpers/admin/roles_helper.rb
@@ -10,7 +10,7 @@ module Admin::RolesHelper
       node[:children].each do |k, n|
         sub << build_action_list_item(k, n)
       end
-      li = %{<li class="checkbox-group"><input type="checkbox" id="action_#{node[:id]}" name="action_#{node[:id]}" /><label class='parent active'><span class='arrow'></span>#{key.capitalize}</label><ul class="sub">#{sub}</ul></li>}
+      li = %{<li class="checkbox-group"><input type="checkbox"  class="parent" id="action_#{node[:id]}" name="action_#{node[:id]}" /><label class='parent active'><span class='arrow'></span>#{key.capitalize}</label><ul class="sub">#{sub}</ul></li>}
     else
       li = %{<li class='checkbox-group'><input type="checkbox" id="action_#{node[:id]}" name="action_#{node[:id]}" /><label>#{key.capitalize}</label></li>}
     end
