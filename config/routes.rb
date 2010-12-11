07fc2142135277dc3ef5a8261f0052664db95054
Tyler Flint <tylerflint@Delorum10.local>
2010-12-11 11:13:38 -0700

    updating configs for rackspace

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,8 @@ Chromada::Application.routes.draw do
   match "privacy"     => "static#privacy",      :as => "privacy"
   match "tou"         => "static#tou",          :as => "tou"
   
-  namespace :admin, :path => "admin", :protocol => (Rails.env.production?)? 'https' : 'http'  do
+  # namespace :admin, :path => "admin", :protocol => (Rails.env.production?)? 'https' : 'http'  do
+  namespace :admin, :path => "admin" do
     root :to                                    => "admin#dashboard"
     match "dashboard"                           => "admin#dashboard",     :as => "dashboard"
     match "companies/:id/dashboard"             => "companies#dashboard", :as => "company_dashboard"
