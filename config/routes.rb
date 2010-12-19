107f2bc2ae36d933602608c6d21b0b9ed5beed35
Tyler Flint <tylerflint@Delorum10.local>
2010-12-18 21:02:52 -0700

    testing a route scope

--- config/routes.rb
+++ config/routes.rb
@@ -10,13 +10,17 @@ Chromada::Application.routes.draw do
     :path_names => { :sign_in => 'login', :sign_out => 'logout', :sign_up => 'signup' }, 
     :controllers => { :sessions => "sessions", :registrations => 'registrations' }
   
-  match "products"    => "static#products",     :as => "products"
-  match "how"         => "static#how",          :as => "how"      
-  match "pricing"     => "static#pricing",      :as => "pricing"
-  match "contact"     => "static#contact",      :as => "contact"                                        
-  match "feedback"    => "static#feedback",     :as => "feedback"
-  match "privacy"     => "static#privacy",      :as => "privacy"
-  match "terms"       => "static#terms",        :as => "terms"
+    scope "", :protocol => 'http' do
+
+      match "products"    => "static#products",     :as => "products"
+      match "how"         => "static#how",          :as => "how"      
+      match "pricing"     => "static#pricing",      :as => "pricing"
+      match "contact"     => "static#contact",      :as => "contact"                                        
+      match "feedback"    => "static#feedback",     :as => "feedback"
+      match "privacy"     => "static#privacy",      :as => "privacy"
+      match "terms"       => "static#terms",        :as => "terms"
+
+    end
   
   namespace :admin, :path => "admin", :protocol => (Rails.env.production?)? 'https' : 'http'  do
     root :to                                    => "admin#dashboard"
