8d25851d230d6905a933363c28ef277b5b524343
Tyler Flint <tylerflint@Delorum10.local>
2010-12-21 19:10:34 -0700

    updating routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Chromada::Application.routes.draw do
   
-  scope "", :protocol => 'http'
-  
   root :to => "static#index", :protocol => 'http'
   
   # sinatra app for quick deploys via post-receive-hooks
@@ -11,14 +9,16 @@ Chromada::Application.routes.draw do
     # :path => "/",
     :path_names => { :sign_in => 'login', :sign_out => 'logout', :sign_up => 'signup' }, 
     :controllers => { :sessions => "sessions", :registrations => 'registrations' }
-  
-  match "products"    => "static#products",     :as => "products"
-  match "how"         => "static#how",          :as => "how"      
-  match "pricing"     => "static#pricing",      :as => "pricing"
-  match "contact"     => "static#contact",      :as => "contact"                                        
-  match "feedback"    => "static#feedback",     :as => "feedback"
-  match "privacy"     => "static#privacy",      :as => "privacy"
-  match "terms"       => "static#terms",        :as => "terms"
+
+  scope "", :protocol => 'http' do
+    match "products"    => "static#products",     :as => "products"
+    match "how"         => "static#how",          :as => "how"      
+    match "pricing"     => "static#pricing",      :as => "pricing"
+    match "contact"     => "static#contact",      :as => "contact"                                        
+    match "feedback"    => "static#feedback",     :as => "feedback"
+    match "privacy"     => "static#privacy",      :as => "privacy"
+    match "terms"       => "static#terms",        :as => "terms"
+  end
   
   namespace :admin, :path => "admin", :protocol => (Rails.env.production?)? 'https' : 'http'  do
     root :to                                    => "admin#dashboard"
