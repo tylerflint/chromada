9068eef647a202645ae6bdb9d0ce3cb71fd8deac
sdomino <domino.steve@gmail.com>
2011-03-03 20:04:44 -0700

    master - NEW: turning off admin access for now

--- config/routes.rb
+++ config/routes.rb
@@ -15,15 +15,15 @@ Chromada::Application.routes.draw do
     match "terms-of-use" => "static#terms"
   end
   
-  namespace :admin, :path => "admin", :protocol => (Rails.env.production?)? 'https' : 'http'  do
-    root :to                                    => "admin#index"
-    match "companies/:id/dashboard"             => "companies#dashboard", :as => "company_dashboard"
-    match "companies/:company_id/users/search"  => "users#search",        :as => "search_company_users"
-    resources :companies do
-      resources :employees
-      resources :permissions
-      resources :users
-    end
-  end
+  # namespace :admin, :path => "admin", :protocol => (Rails.env.production?)? 'https' : 'http'  do
+  #     root :to                                    => "admin#index"
+  #     match "companies/:id/dashboard"             => "companies#dashboard", :as => "company_dashboard"
+  #     match "companies/:company_id/users/search"  => "users#search",        :as => "search_company_users"
+  #     resources :companies do
+  #       resources :employees
+  #       resources :permissions
+  #       resources :users
+  #     end
+  #   end
   
 end
