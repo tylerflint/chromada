3565abfb05ca15b671e0e846bc7108a1e36caf8a
sdomino <domino.steve@gmail.com>
2011-05-31 15:11:38 -0600

    master - NEW: mobile detection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,9 @@ class ApplicationController < ActionController::Base
   
   layout 'site'
   
-  before_filter :check_browser
+  before_filter :check_mobile
   
-  def check_browser
+  def check_mobile
     user_agent = request.env['HTTP_USER_AGENT'].downcase
     if user_agent =~ /mobile|webos|iphone|ipad|palm|blackberry|nokia|portable|sprint|android|motorola|ericsson|mobi/
       redirect_to '/mobile'
